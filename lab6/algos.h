c99c02c6207b017f5b6ebafebfd4950bc8b1834e
ba40k <143427365+ba40k@users.noreply.github.com>
2024-11-06 15:19:33 +0300

    Сделал функции в algos шаблонными

--- lab6/algos.h
+++ lab6/algos.h
@@ -1,14 +1,37 @@
 #pragma once
 
-
-int gcd(int a, int b);
-
-
-void swap(int& a, int& b);
-
-
-int abs(int a);
+template<typename intType>
+intType max(intType a, intType b)
+{
+    return a < b ? b : a;
+}
 
 
-int max(int a, int b);
+template<typename intType>
+void swap(intType& a, intType& b)
+{
+    intType temp = a;
+    a = b;
+    b = temp;
+}
+
+template<typename intType>
+intType abs(intType a)
+{
+    return max(a, a - 2 * a);
+}
+
+template<typename intType>
+intType gcd(intType a, intType b)
+{
+    if (a < b)
+    {
+        swap(a, b);
+    }
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
 
